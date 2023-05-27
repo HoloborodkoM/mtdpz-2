fadb6412117f0da1cd6247a61dad0b090840cfcc
HoloborodkoM <mykhailoholoborodko@gmail.com>
2023-05-27 20:36:20 +0300

    List test: add for methods insert, delete, deleteAll

--- test/List.test.js
+++ test/List.test.js
@@ -64,3 +64,145 @@ describe("List test: method append(element)", () => {
 
   })
 })
+
+describe("List test: method insert(element, index)", () => {
+
+   beforeEach(() => { testList = new List() })
+   
+   test("Checking insert through index", () => {
+      
+      testList.append('i')
+      testList.append('s')
+
+      testList.insert('k', 0)
+      testList.insert('d', 2)
+      testList.insert('?', testList.length())
+
+      expect(testList.length()).toBe(5)
+
+      expect(testList.get(0)).toBe('k')
+      expect(testList.get(1)).toBe('i')
+      expect(testList.get(2)).toBe('d')
+      expect(testList.get(3)).toBe('s')
+      expect(testList.get(4)).toBe('?')
+   
+   })
+   
+   test("Checking insert through incorrect index", () => {
+      
+      testList.append('k')
+
+      const tryInsertCase1 = () => testList.insert('r', -1)
+      const tryInsertCase2 = () => testList.insert('r', testList.length() + 1)
+      
+      expect(testList.length()).toBe(1)
+      
+      expect(tryInsertCase1).toThrow('Error. Incorrect index!!!')
+      expect(tryInsertCase2).toThrow('Error. Incorrect index!!!')
+
+   })
+
+   test("Checking element type", () => {
+      
+      const tryInsertCase1 = () => testList.insert([1, 2, 3], 0)
+      const tryInsertCase2 = () => testList.insert("Ho-ho-ho", testList.length())
+      const tryInsertCase3 = () => testList.insert({add: 'e'}, 0)
+      const tryInsertCase4 = () => testList.insert(3, 0)
+      const tryInsertCase5 = () => testList.append('', 0)
+      
+      expect(testList.length()).toBe(0)
+      
+      expect(tryInsertCase1).toThrow('Error. Expected character type!!!')
+      expect(tryInsertCase2).toThrow('Error. Expected character type!!!')
+      expect(tryInsertCase3).toThrow('Error. Expected character type!!!')
+      expect(tryInsertCase4).toThrow('Error. Expected character type!!!')
+      expect(tryInsertCase5).toThrow('Error. Expected character type!!!')
+
+  })
+})
+
+describe("List test: method delete(element, index)", () => {
+
+   beforeEach(() => { testList = new List() })
+   
+   test("Cheking deleted element through index", () => {
+
+      testList.append('k')
+      testList.append('i')
+      testList.append('d')
+      testList.append('s')
+      
+      const deletedElement1 = testList.delete(0)
+      const deletedElement2 = testList.delete(testList.length() - 1)
+      
+      expect(testList.length()).toBe(2)
+      
+      expect(deletedElement1).toBe('k')
+      expect(deletedElement2).toBe('s')
+
+      expect(testList.get(0)).toBe('i')
+      expect(testList.get(1)).toBe('d')
+   
+   })
+
+   test("Checking delet through incorrect index", () => {
+      
+      testList.append('k')
+
+      const tryDeleteCase1 = () => testList.delete(-1)
+      const tryDeleteCase2 = () => testList.delete(testList.length())
+      
+      expect(testList.length()).toBe(1)
+      
+      expect(tryDeleteCase1).toThrow('Error. Incorrect index!!!')
+      expect(tryDeleteCase2).toThrow('Error. Incorrect index!!!')
+
+   })
+})
+
+describe("List test: method deleteAll(element)", () => {
+
+   beforeEach(() => { testList = new List() })
+   
+   test("Checking to remove all matching elements", () => {
+      
+      testList.append('t')
+      testList.append('k')
+      testList.append('t')
+      testList.append('i')
+      testList.append('t')
+      testList.append('t')
+      testList.append('t')
+      testList.append('d')
+      testList.append('s')
+      testList.append('t')
+      
+      testList.deleteAll('t')
+      
+      expect(testList.length()).toBe(4)
+
+      expect(testList.get(0)).toBe('k')
+      expect(testList.get(1)).toBe('i')
+      expect(testList.get(2)).toBe('d')
+      expect(testList.get(3)).toBe('s')
+   
+   })
+   
+   test("Checking for non-existent element in the list -> do nothing", () => {
+      
+      testList.append('k')
+      testList.append('i')
+      testList.append('d')
+      testList.append('s')
+
+      testList.deleteAll('n')
+
+      expect(testList.length()).toBe(4)
+
+      expect(testList.get(0)).toBe('k')
+      expect(testList.get(1)).toBe('i')
+      expect(testList.get(2)).toBe('d')
+      expect(testList.get(3)).toBe('s')
+
+   })
+})
