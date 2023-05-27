4bb2077ff3b32beed95ac45dd6c6cff3bccbe4a7
HoloborodkoM <mykhailoholoborodko@gmail.com>
2023-05-27 19:39:58 +0300

    List test: add for method append

--- test/List.test.js
+++ test/List.test.js
@@ -27,3 +27,40 @@ describe("List test: method length()", () => {
 
    })
 })
+
+describe("List test: method append(element)", () => {
+
+   beforeEach(() => { testList = new List() })
+  
+   test("Checking the correct addition of elements to the end", () => {
+      
+      testList.append('k')
+      testList.append('i')
+      testList.append('d')
+      testList.append('s')
+      
+      expect(testList.get(0)).toBe('k')
+      expect(testList.get(1)).toBe('i')
+      expect(testList.get(2)).toBe('d')
+      expect(testList.get(3)).toBe('s')
+
+   })
+   
+   test("Checking element type", () => {
+      
+      const tryAddCase1 = () => testList.append(5)
+      const tryAddCase2 = () => testList.append(['1', '2', '3'])
+      const tryAddCase3 = () => testList.append({a: 2})
+      const tryAddCase4 = () => testList.append("Hi there")
+      const tryAddCase5 = () => testList.append('')
+      
+      expect(testList.length()).toBe(0)
+      
+      expect(tryAddCase1).toThrow('Error. Expected character type!!!')
+      expect(tryAddCase2).toThrow('Error. Expected character type!!!')
+      expect(tryAddCase3).toThrow('Error. Expected character type!!!')
+      expect(tryAddCase4).toThrow('Error. Expected character type!!!')
+      expect(tryAddCase5).toThrow('Error. Expected character type!!!')
+
+  })
+})
