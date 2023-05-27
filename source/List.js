63785465d791661c12447e2bf5c9b880d4e7d64d
HoloborodkoM <mykhailoholoborodko@gmail.com>
2023-05-27 04:21:43 +0300

    List: add methods insert, delete and result (helpful)

--- source/List.js
+++ source/List.js
@@ -12,6 +12,37 @@ class List {
       this.list.push(element);
    }
 
+   insert(element, index) {
+
+      if (index < 0 || index > this.list.length) {
+         throw new Error('Error. Incorrect index!!!');
+      }
+   
+      this.list.splice(index, 0, element);
+   }
+
+   delete(index) {
+
+      if (index < 0 || index >= this.list.length) {
+         throw new Error('Error. Incorrect index!!!');
+      }
+
+      const value = this.list[index];
+      this.list.splice(index, 1);
+      return value;
+   }
+
+   result() {
+      
+      let string = "";
+
+      for(const element of this.list) {
+         string += element;
+      }
+
+      return string;
+   }
+
 }
 
 module.exports = List;
